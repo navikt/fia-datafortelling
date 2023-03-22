b01c97f1bcf6b6bad4750908255b7f074a5663c7
Christian Klem <christian.klem@nav.no>
2023-03-22 11:52:32 +0100

    Prøv med ett oppstartsscript
    Co-authored-by: Bettina D'Avila Barros <bettina.d.avila.barros@nav.no>

--- Dockerfile
+++ Dockerfile
@@ -15,5 +15,6 @@ COPY requirements.txt .
 RUN pip3 install -r requirements.txt
 
 COPY main.qmd .
+COPY run.sh .
 
-ENTRYPOINT ["quarto"]
+ENTRYPOINT ["run.sh"]
---
+++ run.sh
@@ -0,0 +1,5 @@
+quarto render main.qmd
+
+curl -X PUT -F file=@index.html \
+    https://${NADA_ENV}/quarto/update/${QUARTO_ID} \
+    -H 'Authorization:Bearer ${QUARTO_TOKEN}'
