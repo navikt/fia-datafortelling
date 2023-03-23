049479a9a3527d1c4b9dfc42612294106cb9819e
Christian Klem <christian.klem@nav.no>
2023-03-23 12:43:16 +0100

    Fjern verbose fra curl

--- run.sh
+++ run.sh
@@ -4,5 +4,4 @@ quarto render main.qmd
 
 curl -X PUT -F file=@main.html \
     https://${NADA_ENV}/quarto/update/${QUARTO_ID} \
-    -H "Authorization:Bearer ${QUARTO_TOKEN}" \
-    --verbose
+    -H "Authorization:Bearer ${QUARTO_TOKEN}"
