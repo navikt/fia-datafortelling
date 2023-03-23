01d14bce2242dceb66cd865356d9f5279c069000
Christian Klem <christian.klem@nav.no>
2023-03-23 12:32:08 +0100

    Sett header med double quotes for å ta i bruk miljøvariabel
    Co-authored-by: Ingrid Fosså <ingrid.fossa@nav.no>

--- run.sh
+++ run.sh
@@ -4,4 +4,5 @@ quarto render main.qmd
 
 curl -X PUT -F file=@main.html \
     https://${NADA_ENV}/quarto/update/${QUARTO_ID} \
-    -H 'Authorization:Bearer ${QUARTO_TOKEN}'
+    -H "Authorization:Bearer ${QUARTO_TOKEN}" \
+    --verbose
