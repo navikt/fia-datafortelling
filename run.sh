e675624c360c1059321fd41f89202c21207b32e4
Christian Klem <christian.klem@nav.no>
2023-03-28 17:48:30 +0200

    last opp alle genererte ressurser

--- run.sh
+++ run.sh
@@ -5,3 +5,7 @@ quarto render main.qmd
 curl -X PUT -F file=@main.html \
     https://${NADA_ENV}/quarto/update/${QUARTO_ID} \
     -H "Authorization:Bearer ${QUARTO_TOKEN}"
+
+find ./main_files -type f -exec curl -X PUT -F file=@{} \
+    https://${NADA_ENV}/quarto/update/${QUARTO_ID} \
+    -H "Authorization:Bearer ${QUARTO_TOKEN}"
