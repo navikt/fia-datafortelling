e3cdc533364e7e260968cd6ff339922c23077890
Bettina D'Avila Barros <bettina.d.avila.barros@nav.no>
2023-05-23 14:05:48 +0200

    Ny datafortelling om datakvalitet
    Co-authored-by: Per-Christian Nielsen <per-christian.nielsen@nav.no>

--- run.sh
+++ run.sh
@@ -5,3 +5,9 @@ quarto render index.qmd
 curl -X PUT -F index.html=@index.html \
     https://${NADA_ENV}/quarto/update/${QUARTO_ID} \
     -H "Authorization:Bearer ${QUARTO_TOKEN}"
+
+quarto render datakvalitet.qmd
+
+curl -X PUT -F index.html=@datakvalitet.html \
+    https://${NADA_ENV}/quarto/update/${QUARTO_ID_DATAKVALITET} \
+    -H "Authorization:Bearer ${QUARTO_TOKEN}"
