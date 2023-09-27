dbf87d07ad97a2aeed41daa6d2d2642953b68038
Per-Christian Nielsen <per-christian.nielsen@nav.no>
2023-09-27 08:48:03 +0200

    Kjører og laster opp quarto for alle fylker (igjen)

--- run.sh
+++ run.sh
@@ -2,7 +2,15 @@
 
 quarto render index.qmd
 
-curl -X PUT -F index.html=@index.html \
+FILER="-F index.html=@index.html"
+for FYLKESNAVN in "Oslo" "Rogaland" "Møre og Romsdal" "Nordland" "Vest-Viken" "Øst-Viken" "Innlandet" "Vestfold og Telemark" "Agder" "Vestland" "Trøndelag" "Troms og Finnmark"
+do
+    export FILNAVN=`echo "datafortelling_per_fylke_$FYLKESNAVN.html" | sed 's/ /_/g'`
+    quarto render datafortelling_per_fylke.qmd --output $FILNAVN
+    FILER="$FILER -F $FILNAVN=@$FILNAVN"
+done
+
+curl -X PUT $FILER \
     https://${NADA_ENV}/quarto/update/${QUARTO_ID} \
     -H "Authorization:Bearer ${QUARTO_TOKEN}"
 
