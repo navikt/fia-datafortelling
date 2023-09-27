e4f0dcf7cf2ef38b1afda7401091f8ab9f588b3d
Per-Christian Nielsen <per-christian.nielsen@nav.no>
2023-09-27 09:27:28 +0200

    Exporterer FYLKESNAVN for å få tak i den inni quarto

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ quarto render index.qmd
 FILER="-F index.html=@index.html"
 for FYLKESNAVN in "Oslo" "Rogaland" "Møre og Romsdal" "Nordland" "Vest-Viken" "Øst-Viken" "Innlandet" "Vestfold og Telemark" "Agder" "Vestland" "Trøndelag" "Troms og Finnmark"
 do
+    export FYLKESNAVN
     export FILNAVN=`echo "datafortelling_per_fylke_$FYLKESNAVN.html" | sed 's/ /_/g'`
     quarto render datafortelling_per_fylke.qmd --output $FILNAVN
     FILER="$FILER -F $FILNAVN=@$FILNAVN"
