8864cf027f4b5b0b5721a40878be2d94a64a8eb6
Bettina D'Avila Barros <bettina.d.avila.barros@nav.no>
2023-09-27 06:16:43 +0200

    Revert "Kjør datafortelling for hvert fylke"
    This reverts commit 342058ffc9e905fba234052d98add901b8904985.

--- run.sh
+++ run.sh
@@ -6,15 +6,6 @@ curl -X PUT -F index.html=@index.html \
     https://${NADA_ENV}/quarto/update/${QUARTO_ID} \
     -H "Authorization:Bearer ${QUARTO_TOKEN}"
 
-for FYLKE in "Oslo" "Rogaland" "Møre og Romsdal" "Nordland" "Vest-Viken" "Øst-Viken" "Innlandet" "Vestfold og Telemark" "Agder" "Vestland" "Trøndelag" "Troms og Finnmark"
-do 
-    export FYLKESNAVN=$FYLKE
-    quarto render datafortelling_per_fylke.qmd --output "datafortelling_per_fylke_$FYLKESNAVN.html"
-    curl -X PUT -F datafortelling_per_fylke_$FYLKESNAVN.html=@datafortelling_per_fylke_$FYLKESNAVN.html \
-        https://${NADA_ENV}/quarto/update/${QUARTO_ID} \
-        -H "Authorization:Bearer ${QUARTO_TOKEN}"
-done
-
 quarto render datakvalitet.qmd
 
 curl -X PUT -F index.html=@datakvalitet.html \
