159da362417469e2d09f5abf2aff42a4061370a7
Bettina D'Avila Barros <bettina.d.avila.barros@nav.no>
2023-11-13 07:59:11 +0100

    Opprett ny datafortelling "PIA nøkkeltall"

--- run.sh
+++ run.sh
@@ -20,3 +20,9 @@ quarto render datakvalitet.qmd
 curl -X PUT -F index.html=@datakvalitet.html \
     https://${NADA_ENV}/quarto/update/${QUARTO_ID_DATAKVALITET} \
     -H "Authorization:Bearer ${QUARTO_TOKEN}"
+
+quarto render teampia.qmd
+
+curl -X PUT -F index.html=@teampia.html \
+    https://${NADA_ENV}/quarto/update/${QUARTO_ID_TEAMPIA} \
+    -H "Authorization:Bearer ${QUARTO_TOKEN}"
