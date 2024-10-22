9c903ea530d6ddbc2a6ddf2ad99c0a19fb78e7a0
Bettina D'Avila Barros <bettina.d.avila.barros@nav.no>
2024-10-22 15:22:23 +0200

    Kjør kun teampia-datafortelling, for å lettere finne feilen i naisjoben

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+: '
 quarto render helsesjekk.qmd
 
 curl -X PUT -F index.html=@helsesjekk.html \
@@ -26,7 +26,7 @@ quarto render datakvalitet.qmd
 curl -X PUT -F index.html=@datakvalitet.html \
     https://${NADA_ENV}/quarto/update/${QUARTO_ID_DATAKVALITET} \
     -H "Authorization:Bearer ${QUARTO_TOKEN}"
-
+'
 quarto render teampia.qmd
 
 curl -X PUT -F index.html=@teampia.html \
