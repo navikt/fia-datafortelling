9a705f02eb6ba646e45e969e8c56db2558c8a947
Bettina D'Avila Barros <bettina.d.avila.barros@nav.no>
2024-11-08 08:22:02 +0100

    Skjult IA-tjenester fra områdesidene

--- run.sh
+++ run.sh
@@ -15,8 +15,13 @@ FILER="$FILER -F ia_tjenester.html=@ia_tjenester.html"
 for RESULTATOMRADE in "Oslo" "Rogaland" "Møre og Romsdal" "Nordland" "Vest-Viken" "Øst-Viken" "Innlandet" "Vestfold og Telemark" "Agder" "Vestland" "Trøndelag" "Troms og Finnmark"
 do
     export RESULTATOMRADE
+
     export FILNAVN=`echo "datafortelling_per_resultatområde_$RESULTATOMRADE.html" | sed 's/ /_/g'`
     quarto render datafortelling_per_resultatområde.qmd --output $FILNAVN
+    FILER="$FILER -F $FILNAVN=@$FILNAVN"
+
+    export FILNAVN=`echo "ia_tjenester_per_resultatområde_$RESULTATOMRADE.html" | sed 's/ /_/g'`
+    quarto render ia_tjenester_per_resultatområde.qmd --output $FILNAVN
     FILER="$FILER -F $FILNAVN=@$FILNAVN"
 done
 
